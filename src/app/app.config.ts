98397a8cda91e163be17e11056bd70f8300535cd
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-04-02 09:11:26 +0200

    init firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"notes-66dc3","appId":"1:579854407537:web:09f9957e59732be2f888d0","storageBucket":"notes-66dc3.firebasestorage.app","apiKey":"AIzaSyAw9ItJmfl4vwRM1LqJZTPIhZoX1jpZve8","authDomain":"notes-66dc3.firebaseapp.com","messagingSenderId":"579854407537"}))),
+    importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
